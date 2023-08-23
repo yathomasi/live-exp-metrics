9b0f00364ad7a24cb3480f79b86c8c5e08688d66
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2023-08-23 14:05:54 +0545

    super nested

--- hello.py
+++ hello.py
@@ -6,10 +6,10 @@ from dvclive import Live
 
 params = dvc.api.params_show()
 
-with Live(dir="hello/usage") as live:
+with Live(dir="new/model/config/trainer/params") as live:
     for i in range(params["epochs"]):
-        live.log_metric("bar", i + random.random())
-        live.log_metric("foo", i + random.random())
+        live.log_metric("node1", i + random.random())
+        live.log_metric("node2", i + random.random())
         live.next_step()
 
         time.sleep(5)
