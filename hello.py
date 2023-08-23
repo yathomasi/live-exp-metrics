bbb85dd0da0eb1b2f0cb65f0190feda818efe44e
Thomas Kunwar <20840228+yathomasi@users.noreply.github.com>
2023-08-23 12:34:19 +0545

    new exp

---
+++ hello.py
@@ -0,0 +1,18 @@
+import time
+import random
+
+import dvc.api
+from dvclive import Live
+
+params = dvc.api.params_show()
+
+with Live(dir="hello/world") as live:
+    for i in range(params["epochs"]):
+        live.log_metric("bar", i + random.random())
+        live.log_metric("foo", i + random.random())
+        live.next_step()
+
+        time.sleep(5)
+
+
+live.end()
